3cdfc748ac2b5203a94b02761cbfabdf2e422abe
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-08-22 15:38:36 +0530

    added name field

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.justjava;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         CheckBox whippedCream= findViewById(R.id.chkbox_whippedcream);
         CheckBox chocolate= findViewById(R.id.chocolate_checkbox);
+        EditText nameField= findViewById(R.id.name_field);
+        String name=nameField.getText().toString();
         boolean haswhippedCream= whippedCream.isChecked();
         boolean haschocolate= chocolate.isChecked();
         int price=calculatePrice(noc);
-        displayMessage(createOrderSummary(price,haswhippedCream,haschocolate));
+        displayMessage(createOrderSummary(price,haswhippedCream,haschocolate,name));
     }
 
     public void increment(View view) {
@@ -49,14 +53,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String createOrderSummary(int price,boolean hasCream,boolean haschoco){
-        String msg = "Name: Arpit Chauhan\nQuantity: "+ noc;
+    private String createOrderSummary(int price,boolean hasCream,boolean haschoco,String nm){
+        String msg = "Name: "+nm;
+        msg+="\nQuantity: "+ noc;
         msg+="\nAdded Whipped Cream ? "+ hasCream;
         msg+="\nAdded Chocolate ? "+ haschoco;
         msg+="\nTotal = ₹" + price;
         msg += "\nThank You!";
         return msg;
     }
+
 
 
     /**
