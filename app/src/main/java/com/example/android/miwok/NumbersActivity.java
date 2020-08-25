7b47cba49ce7cd5633d03e7f24a0d173007ab923
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-08-25 22:35:12 +0530

    New project

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -27,5 +27,8 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String words[]={"one","two","three","four","five","six","seven","eight","nine","ten"};
+
     }
 }
